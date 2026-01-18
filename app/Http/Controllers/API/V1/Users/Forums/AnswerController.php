0344c00f36c122c903e91d6ef046c20733ad684f
tnnq <0306231146@caothang.edu.vn>
2026-01-18 20:29:27 +0700

    Tao API cho answer va lay answer theo question_id

--- app/Http/Controllers/API/V1/Users/Forums/AnswerController.php
+++ app/Http/Controllers/API/V1/Users/Forums/AnswerController.php
@@ -18,9 +18,23 @@ class AnswerController extends BaseCRUDController
         return [
            'content'   => 'required|string',
             'question_id' => 'required|integer|exists:questions,id',
-            'parent_id' => 'required|integer|exists:comments,id',
+            'parent_id' => 'nullable|integer|exists:answers,id',
             'user_id'   => 'required|integer|exists:users,id',
             'status'    => 'required|in:active,inactive'
         ];
+    }
+    public function listByQuestionId($id)
+    {
+        // Lấy các câu trả lời cấp 1 (parent_id = null) của câu hỏi theo question_id
+        // Đồng thời load thông tin user và các câu trả lời con (đa cấp)
+       $data = $this->model::with([
+            'user',
+            'answersRecursive'
+        ])
+        ->where('question_id', $id)
+        ->whereNull('parent_id')
+        ->get();
+
+        return $this->sendResponse($data, 'Lấy danh sách câu trả lời thành công');
     }
 }
